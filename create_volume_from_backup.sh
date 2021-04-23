83181ed0b8870e6770a719322b4a9dcfd00be62b
trodevel <trodevel@gmail.com>
2021-04-23 04:22:25 +0200

    added create_volume_from_backup.sh

---
+++ create_volume_from_backup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#<hb>***************************************************************************
+#
+# create docker volume from archive
+#
+# USAGE: create_volume_from_backup.sh <archive_name.tar.gz> <volume_name> [-V]
+#
+# Example: create_volume_from_backup.sh backup.tar.gz super_volume
+#
+#<he>***************************************************************************
+
+show_help()
+{
+    sed -e '1,/^#<hb>/d' -e '/^#<he>/,$d' $0 | cut -c 3-
+}
+
+FL=$1
+VOLUME=$2
+IS_VERBOSE=$3
+
+[[ -z $FL ]]     && echo "ERROR: filename is not given" && show_help && exit
+[[ -z $VOLUME ]] && echo "ERROR: volume name is not given" && show_help && exit
+
+[[ ! -f $FL ]]   && echo "ERROR: file '$FL' does not exist" && show_help && exit
+
+docker volume inspect $VOLUME >/dev/null 2>&1
+err=$?
+
+[[ $err -eq 0 ]] && echo "ERROR: volume '$VOLUME' already exists" && exit $err
+
+echo "INFO: create volume '$VOLUME' from $FL"
+
+v_flag=""
+
+[[ -n $IS_VERBOSE ]] && v_flag="v"
+
+cat $FL | docker run --rm -i -v $VOLUME:/to alpine ash -c "cd /to ; tar -xp${v_flag}f - "
+err=$?
+
+[[ $err -ne 0 ]] && echo "ERROR: $err - failed to create volume '$VOLUME'" && exit $err
+
+echo "INFO: create volume '$VOLUME' from $FL - done"
